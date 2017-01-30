d7dc8e504a2a4a5da7a8d20712064a7f1bddb9da
Aran Dhaliwal <aran.dhaliwal@imperial.ac.uk>
2017-01-30 16:56:16 +0000

    added constraint scraping

--- planning/spiders/app_spider.py
+++ planning/spiders/app_spider.py
@@ -9,12 +9,20 @@ class ApplicationSpider(scrapy.Spider):
 
     
     def parse(self,response):            
-        for table in response.xpath("/html/body/div/div/div[2]/div[3]/div[3]/table"):
-            yield {
-                "address" : table.xpath("//tr[5]/td/text()").extract(),
-                "proposal": table.xpath("//tr[6]/td/text()").extract(),
-                "decision": table.xpath("//tr[8]/td/text()").extract(),
-            }
+        table = response.xpath("/html/body/div/div/div[2]/div[3]/div[3]/table")
+        yield {
+            "address" : table.xpath("//tr[5]/td/text()").extract(),
+            "proposal": table.xpath("//tr[6]/td/text()").extract(),
+            "decision": table.xpath("//tr[8]/td/text()").extract(),
+        }
             
-        contraintspage = response.css("ul.tabs a::attr(href)")[5].extract()
+        constraintspage = response.css("ul.tabs a::attr(href)")[5].extract()
         constraintspage = response.urljoin(constraintspage)
+        yield scrapy.Request(constraintspage, callback=self.parse_constraints)
+        
+    def parse_constraints(self,response):
+        table = response.xpath("/html/body/div/div/div[2]/div[3]/div[3]/table")
+        yield {
+            "constraints" : table.xpath("//tr/td[1]/text()").extract()
+        }
+        
