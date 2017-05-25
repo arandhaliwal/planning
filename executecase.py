38c6606f35dec03120722ab93379d0d982e15754
Aran Dhaliwal <aran.dhaliwal@imperial.ac.uk>
2017-05-25 21:32:20 +0100

    improved explanation, think it is working

--- executecase.py
+++ executecase.py
@@ -3,18 +3,20 @@ import re
 
 wordlist = getKeywords()
 
-casebase = buildCasebase(wordlist)
+#casebase = buildCasebase(wordlist)
 
-newcase = getNewCase(wordlist)
+#newcase = getNewCase(wordlist)
 
-'''case1 = Case([],"plus",[],[])
+case1 = Case([],"plus",[],[])
 case2 = Case(["S"],"minus",[],[])
 case3 = Case(["S","O"],"plus",[],[])
 case4 = Case(["S","E"],"plus",[],[])
 case5 = Case(["S","E","O"],"minus",[],[])
 case6 = Case(["S","E","O","M"],"plus",[],[])
-casebase = [case1,case2,case3,case4,case5]
-newcase = Case(["S","E","O","G"],"unknown",[],[])'''
+case7 = Case(["S","E","O","G"],"plus",[],[])
+case8 = Case(["S","E","O","G","M"],"minus",[],[])
+casebase = [case1,case2,case3,case4,case5,case6,case7,case8]
+newcase = Case(["S","E","O","G","M","P"],"unknown",[],[])
 
 print("Prediction:")
 print(computePrediction(newcase,casebase))
@@ -34,36 +36,50 @@ if "newcase" in groundedextension:
 
 #default is plus, outcome is minus
 
-print("explanation\n")
+print("\nexplanation:\n")
 
-def recursivefunctionagree(tree,case):
-    nextcase = case.attackedby[0]
+def recursivefunctionagree(tree,case,count):
+    treecopy = list(tree)
+    nextcase = case.attackedby[count]
     if nextcase.attackedby == [] or newcase in nextcase.attackedby:
-        return tree
+        return treecopy
     else:
-        tree.append(nextcase)
-        return recursivefunctiondisagree(tree,nextcase)
+        treecopy.append(nextcase)
+        return recursivefunctiondisagree(treecopy,nextcase)
 
 def recursivefunctiondisagree(tree,case):
+    treecopy = list(tree)
     for nextcase in ge:
         if case in nextcase.attacks:
-            tree.append(nextcase)
+            treecopy.append(nextcase)
             break
     if nextcase.attackedby == []:
-        return tree
+        return treecopy
     else:
-        return recursivefunctionagree(tree,nextcase)
-    
+        anothertreeslist = []
+        count = 0
+        for next2case in nextcase.attackedby:
+            anothertreeslist.append(recursivefunctionagree(treecopy,nextcase,count))
+            count += 1
+        return anothertreeslist
 
 for case in ge:
     if casebase[0] in case.attacks:
-        tree = [casebase[0],case]
-        tree = recursivefunctionagree(tree,case)
-        
-for case in tree:
-    pprint(case.args)
-    pprint(case.outcome)
-    if tree[len(tree)-1] != case:
-        print("\nis attacked by...")
-    else:
-        print("\nwhich is unattacked")
+        treebase = [casebase[0],case]
+        treeslist = []
+        count = 0
+        for nextcase in case.attackedby:
+            treeslist.append(recursivefunctionagree(treebase,case,count))
+            count += 1
+
+print(treeslist)            
+'''for tree in treeslist:            
+    for case in tree:
+        pprint(case.args)
+        pprint(case.outcome)
+        if tree[len(tree)-1] != case:
+           print("\nis attacked by...\n")
+        else:
+            print("\nwhich is unattacked\n")
+    if treeslist[len(treeslist)-1] != tree:
+        print("\nOR\n")'''
